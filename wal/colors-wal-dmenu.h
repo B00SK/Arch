93fec41298af70d80c24d99f63ab939ac873f1e3
B00SK <aphrac4@gmail.com>
2026-07-14 17:42:25 +0200

    remove wal cache

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e4b9b2", "#0c060f" },
-	[SchemeSel] = { "#e4b9b2", "#991634" },
-	[SchemeOut] = { "#e4b9b2", "#D85847" },
-};
--- wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0c060f";
-static const char* selfgcolor   = "#e4b9b2";
-static const char* normbgcolor  = "#E30E2F";
-static const char* normfgcolor  = "#e4b9b2";
-static const char* urgbgcolor   = "#991634";
-static const char* urgfgcolor   = "#e4b9b2";
